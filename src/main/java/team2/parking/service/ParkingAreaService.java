9bb7cd663c8483175ced04e2fb5ddb93837f26eb
CJ-Park <king01286@naver.com>
2024-12-13 10:27:47 +0900

    feat: ParkingAreaService - 전체 주차공간 및 사용 가능 주차공간 카운트 메서드 추가

--- src/main/java/team2/parking/service/ParkingAreaService.java
+++ src/main/java/team2/parking/service/ParkingAreaService.java
@@ -2,7 +2,7 @@
 * Parking Lot Management Service
 * 주차 구역 서비스
 * 작성자 : semi_lion2 (고민정, 박창조, 이홍비, 허선호)
-* 최종 수정 날짜 : 2024.12.12
+* 최종 수정 날짜 : 2024.12.13
 *
 * ========================================================
 * 프로그램 수정 / 보완 이력
@@ -10,6 +10,7 @@
 * 작업자       날짜       수정 / 보완 내용
 * ========================================================
 * 고민정    2024.12.12    ParkingArea 서비스 생성
+* 박청조    2024.12.13    사용 가능한 주차공간 및 전체 주차 공간 카운트 메서드 추가
 * ========================================================
 */ 
 
@@ -34,5 +35,15 @@ public class ParkingAreaService {
 									.map(ParkingAreaDto::from)
 									.orElseThrow();
 		
+	}
+
+	// 사용 가능한 주차 공간
+    public int getUsableAreaCount() {
+		return (int) parkingAreaRepository.countParkingAreasByInUseIsTrue();
+	}
+
+	// 전체 주차 공간
+	public int getTotalAreaCount() {
+		return (int) parkingAreaRepository.count();
 	}
 }
