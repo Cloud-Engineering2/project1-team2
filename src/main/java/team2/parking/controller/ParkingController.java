e05b430bfcdade4ecc148c7bd6383e30ca1ded61
bbbng <heoseonho@gmail.com>
2024-12-13 11:22:31 +0900

    feat: ParkingController: 주차 현황 기능 추가

--- src/main/java/team2/parking/controller/ParkingController.java
+++ src/main/java/team2/parking/controller/ParkingController.java
@@ -10,19 +10,23 @@
 * 작업자       날짜       수정 / 보완 내용
 * ========================================================
 * 허선호    2024.12.12    더미 데이터를 이용한 getParkingrecordByVehicleno 메소드 작성 
-* 
+* 허선호    2024.12.13    getParkingstatus 주차 현황을 표시하는 메소드 작성
 * 
 * ========================================================
 */ 
 
 package team2.parking.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.AllArgsConstructor;
+import team2.parking.dto.ParkingAreaDto;
 import team2.parking.dto.ParkingRecordDto;
 import team2.parking.service.ParkingService;
 
@@ -41,6 +45,19 @@ public class ParkingController {
 		model.addAttribute("record", record);
 		return view;
 		
+	}
+	
+	// getParkingstatus 주차 현황을 표시하는 메소드 작성(인트로페이지 첫화면)
+	@GetMapping(value = "/")
+	public String getParkingstatus(@RequestParam(value="zone", 
+		    required = false, defaultValue="A") String zone, Model model) {
+		String view = "/index.html";
+
+		List<ParkingAreaDto> status = parkingService.getParkingstatus(zone);
+		
+		model.addAttribute("status", status);
+		model.addAttribute("zone", zone);
+		return view;
 	}
 	
 }
--- src/main/java/team2/parking/service/ParkingService.java
+++ src/main/java/team2/parking/service/ParkingService.java
@@ -10,6 +10,7 @@
 * 작업자       날짜       수정 / 보완 내용
 * ========================================================
 * 허선호    2024.12.12    ParkingService 서비스 생성
+* 허선호    2024.12.13    getParkingstatus() 구역내 주차상황 확인 메소드 생성
 * ========================================================
 */ 
 
@@ -19,9 +20,12 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import ch.qos.logback.classic.net.SyslogAppender;
 import lombok.RequiredArgsConstructor;
+import team2.parking.dto.ParkingAreaDto;
 import team2.parking.dto.ParkingRecordDto;
 import team2.parking.dto.VehicleDto;
+import team2.parking.repository.ParkingAreaRepository;
 import team2.parking.repository.ParkingRecordRepository;
 import team2.parking.repository.VehicleRepository;
 
@@ -30,7 +34,9 @@ import team2.parking.repository.VehicleRepository;
 public class ParkingService {
     private final ParkingRecordRepository parkingRecordRepository;
     private final VehicleRepository vehicleRepository;
+    private final ParkingAreaRepository parkingAreaRepository;
 
+    // getParkingrecordByVehicleno: 차량 번호로 주차된 장소 및 정보 찾기 메소드
 	public ParkingRecordDto getParkingrecordByVehicleno(String vehicleno) {
 //		검색창에 입력한 차량 번호와 일치하는 VehicleDto 찾기
     	VehicleDto vehicleDto = vehicleRepository.findByvNumberContaining(vehicleno)
@@ -42,11 +48,17 @@ public class ParkingService {
 
 
 		// 위에서 찾은 주차기록에서 VehicleDto로 필터링해 반환
-		System.out.println(vehicleDto);
-		System.out.println(parkingRecordDtos);
 		ParkingRecordDto result = parkingRecordDtos.stream()
 				.filter(r -> r.getVehicleId().getId().equals(vehicleDto.getId()))
 				.findFirst().orElseThrow(() -> new IllegalArgumentException("요청하신 차량은 현재 주차 중이 아닙니다."));
 		return result;
+	}
+	
+	// 구역 내 주차상황 확인
+	public List<ParkingAreaDto> getParkingstatus(String zone) {
+		// 현재 주차 중인 기록 찾기
+		List<ParkingAreaDto> parkingStatus = parkingAreaRepository.findAll()
+				.stream().map(ParkingAreaDto::from).filter(dto -> dto.getLocation().charAt(0) == zone.charAt(0)).toList();
+		return parkingStatus;
 	}
 }
